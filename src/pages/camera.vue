de93addae21dc6191981f41958bcdd3919d2479e
meng <1739462844@qq.com>
2018-12-28 12:42:25 +0800

    camera

--- src/pages/camera.vue
+++ src/pages/camera.vue
@@ -1,85 +1,96 @@
 <template>
-  <div class="wh-100 absolute">
-    <video id="video"></video>
-    {{stream}}
-    <div @click="liveVideo" class="camera-btn">
-      <div class="camera-btn-div"></div>
+    <div class="wh-100 absolute">
+        <!-- <video id="video"></video>
+        {{stream}}-->
+        <camera-item class="camera-container"></camera-item>
+        <!-- <div class="camera-btn">
+            <div class="camera-btn-div"></div>
+        </div> -->
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      stream: "1212"
-    };
-  },
-  created() {},
-  methods: {
-    liveVideo() {
-      function getUserMedia(constrains, success, error) {
-        if (navigator.mediaDevices.getUserMedia) {
-          //最新标准API
-          navigator.mediaDevices
-            .getUserMedia(constrains)
-            .then(success)
-            .catch(error);
-        } else if (navigator.webkitGetUserMedia) {
-          //webkit内核浏览器
-          navigator
-            .webkitGetUserMedia(constrains)
-            .then(success)
-            .catch(error);
-        } else if (navigator.mozGetUserMedia) {
-          //Firefox浏览器
-          navagator
-            .mozGetUserMedia(constrains)
-            .then(success)
-            .catch(error);
-        } else if (navigator.getUserMedia) {
-          //旧版API
-          navigator
-            .getUserMedia(constrains)
-            .then(success)
-            .catch(error);
-        }
-      }
+/* eslint-disable */
+import cameraItem from "./cameraItem"
 
-      getUserMedia(
-        {
-          video: { width: 480, height: 320 }
-        },
-        res => {
-          console.log(res);
-        },
-        err => {
-          console.log(err);
-        }
-      );
+export default {
+    components: { cameraItem },
+    data() {
+        return {
+            //stream: "1212"
+        };
+    },
+    created() {},
+    methods: {
+        // liveVideo() {
+        //     function getUserMedia(constrains, success, error) {
+        //         if (navigator.mediaDevices.getUserMedia) {
+        //             //最新标准API
+        //             navigator.mediaDevices
+        //                 .getUserMedia(constrains)
+        //                 .then(success)
+        //                 .catch(error);
+        //         } else if (navigator.webkitGetUserMedia) {
+        //             //webkit内核浏览器
+        //             navigator
+        //                 .webkitGetUserMedia(constrains)
+        //                 .then(success)
+        //                 .catch(error);
+        //         } else if (navigator.mozGetUserMedia) {
+        //             //Firefox浏览器
+        //             navagator
+        //                 .mozGetUserMedia(constrains)
+        //                 .then(success)
+        //                 .catch(error);
+        //         } else if (navigator.getUserMedia) {
+        //             //旧版API
+        //             navigator
+        //                 .getUserMedia(constrains)
+        //                 .then(success)
+        //                 .catch(error);
+        //         }
+        //     }
+
+        //     getUserMedia(
+        //         {
+        //             video: { width: 480, height: 320 }
+        //         },
+        //         res => {
+        //             console.log(res);
+        //         },
+        //         err => {
+        //             console.log(err);
+        //         }
+        //     );
+        // }
     }
-  }
 };
 </script>
 
  <style lang='scss' scoped>
-.camera-btn {
-  position: absolute;
-  bottom: 1.97rem;
-  left: 0;
-  right: 0;
-  margin: auto;
-  width: 2.15rem;
-  height: 2.15rem;
-  border-radius: 50%;
-  border: 0.1rem solid #00587f;
-  padding: 0.15rem;
-}
-.camera-btn-div {
-  width: 100%;
-  height: 100%;
-  background: #00587f;
-  border-radius: 50%;
-}
+ .camera-container{
+     position: absolute;
+     top: 20%;
+     left: 50%;
+     transform: translateX(-50%);
+ }
+// .camera-btn {
+//     position: absolute;
+//     bottom: 1.97rem;
+//     left: 0;
+//     right: 0;
+//     margin: auto;
+//     width: 2.15rem;
+//     height: 2.15rem;
+//     border-radius: 50%;
+//     border: 0.1rem solid #00587f;
+//     padding: 0.15rem;
+// }
+// .camera-btn-div {
+//     width: 100%;
+//     height: 100%;
+//     background: #00587f;
+//     border-radius: 50%;
+// }
 </style>
 
---
+++ src/pages/cameraItem.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <div class="camera-outter">
+            <div class="camera-inner"></div>
+            <div class="camera-item"></div>    
+            <div class="camera-item"></div>    
+            <div class="camera-item"></div>    
+            <div class="camera-item"></div>    
+        </div>
+        <div class="camera-btn">
+            <div class="camera-btn-div"></div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'CameraItem',
+    props: '',
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+$browser-default-font-size: 108px !default; //变量的值可以根据自己需求定义 /10
+@function pxTorem($px) {
+//$px为需要转换的字号
+@return $px/$browser-default-font-size*1rem;
+}
+.container{
+    width: pxTorem(600px);
+    height: pxTorem(1200px);
+    border: 1px solid #000;
+    .camera-btn{
+        position: absolute;
+        width: pxTorem(240px);
+        height: pxTorem(240px);
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        border-radius: 50%;
+        border: pxTorem(10px) solid #00587f;
+        padding: pxTorem(15px);
+        .camera-btn-div{
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #00587f;
+        }
+    }
+}
+</style>
+
+
